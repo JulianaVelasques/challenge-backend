6666baed95f15363703867007611d67cef9a3f4f
JulianaVelasques <julianabalta16@gmail.com>
2025-03-22 18:17:09 -0300

    feat(cart): add CartItem model and associations
    - Create CartItem model with references to cart and product
    - Create migration adding cart_items table
    - Update Cart model to include has_many relationships
    - Update schema.rb to reflect the new migration

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,9 @@
 class Cart < ApplicationRecord
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
+  # A cart can have multiple cartItem, if a cart is destroyed, then all the cartItem in this cart should be too
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
+
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
 end
